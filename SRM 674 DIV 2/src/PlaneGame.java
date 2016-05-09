5d0d4c9bf759d94b29d7ebfd8e54c4e0c0c8a78e
jrathor <jrathor@microsoft.com>
2016-05-09 19:25:10 +0530

    SRM 674Div2

--- SRM 674 DIV 2/src/PlaneGame.java
+++ SRM 674 DIV 2/src/PlaneGame.java
@@ -4,7 +4,45 @@ import static java.lang.Math.*;
 
 public class PlaneGame {
 	
-	public int bestShot(int[] x, int[] y) {
-		return 0;
+	public int bestShot(int[] x, int[] y)
+	{
+		if(x.length<=2)
+			return x.length;
+
+		int maxCount = -1;
+		for(int i=0,len = x.length;i<len;i++)
+		{
+			for(int j=0;j<len;j++)
+			{
+				if(i!=j)
+				{
+					int slpx12 = x[j] - x[i];
+					int slpy12 = y[j] - y[i];
+					for (int k = 0; k < len; k++)
+					{
+						if(k!=i && k!=j)
+						{
+							int cnt = 0;
+							for(int l = 0;l<len;l++)
+							{
+								int slpx14 = x[l] - x[i];
+								int slpy14 = y[l] - y[i];
+								if(slpx12 * slpy14 == slpy12 * slpx14)
+									cnt++;
+								else
+								{
+									int slpx34 = x[l] - x[k];
+									int slpy34 = y[l] - y[k];
+									if(slpx12 * slpy34 == -slpy12 * slpx34)
+										cnt++;
+								}
+							}
+							maxCount = max(maxCount,cnt);
+						}
+					}
+				}
+			}
+		}
+		return maxCount;
 	}
 }
